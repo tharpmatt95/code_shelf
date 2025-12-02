8f145ef584a6caa57f4086d1478a1d3ab2aba5f9
Matt Tharp <tharpmatt95@proton.me>
2025-12-02 12:49:33 -0800

    Environment variable for Vite

--- src/main.js
+++ src/main.js
@@ -8,7 +8,8 @@ import 'element-plus/dist/index.css'
 
 import router from './router'
 
-axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || 'http://localhost:4000';
 axios.defaults.withCredentials = true
 
 createApp(App)
